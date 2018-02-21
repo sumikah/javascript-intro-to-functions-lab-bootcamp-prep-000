795be2947529560198a99b46f51c4cbe8402c9f0
SUMIKA HAZAMA <sumikah@hotmail.com>
2018-02-21 06:58:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function logWhisper(){
 }
 
 function sayHiToGrandma(string){
+<<<<<<< HEAD
 
   if( string.toLowerCase() === string){
     return "I can\'t hear you!"
@@ -26,3 +27,14 @@ function sayHiToGrandma(string){
   else if ( string.toUpperCase() === string){
     return "YES INDEED!"
   }}
+=======
+  if( string.toLowerCase() === lowercase){
+    return "I can\'t hear you!"
+  }
+  else if( string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+  else if ( string.toUpperCase() === uppercase){
+    return "YES, INDEED!"
+  } else(string)}
+>>>>>>> 666560abb8ffe21eaf6e84c9c3c85c4cacfbef43
